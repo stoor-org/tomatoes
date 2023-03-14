b2730e7d9fd182d6edf1199d1e2332afb063131b
shirawildp <125363722+shirawildp@users.noreply.github.com>
2023-03-14 15:43:19 +0200

    Create packages.js

---
+++ packages.js
@@ -0,0 +1,19 @@
+{
+  "name": "tomatoes",
+  "version": "1.0.0",
+  "description": "champagne for everyone!",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/stoor-org/tomatoes.git"
+  },
+  "author": "",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/stoor-org/tomatoes/issues"
+  },
+  "homepage": "https://github.com/stoor-org/tomatoes#readme"
+}
