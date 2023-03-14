136da8569f3bbfef39b73edb6f6b29717ccf3a94
shira2r <65590254+shira2r@users.noreply.github.com>
2023-03-14 16:57:32 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 import sys, requests
 
-#url = "https://httpdump.app/dumps/58d8f12e-64c2-4a27-8827-67a2749fd4dc/" + sys.argv[1]
-url = "https://httpdump.app/dumps/58d8f12e-64c2-4a27-8827-67a2749fd4dc/ABCD"
+url = "https://httpdump.app/dumps/58d8f12e-64c2-4a27-8827-67a2749fd4dc/" + sys.argv[1]
+#url = "https://httpdump.app/dumps/58d8f12e-64c2-4a27-8827-67a2749fd4dc/ABCD"
 response = requests.get(url)
